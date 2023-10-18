0691aeb091c1a3903bf60e35bf73ad0a464710d5
Shannon Schrauwen <shannon.schrauwen@gmail.com>
2023-10-18 07:39:38 +0300

    text fixes

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
     { label: "My Education", link: "education" },
     {
       label: "My CV",
-      link: "https://drive.google.com/file/d/1KiLOfF7LfTFlXVGzIstLskMhcj_D5GRJ/view?usp=sharing",
+      link: "https://drive.google.com/file/d/1eGUAP-e7MJMygEB-0br_OTLprC1g2b4s/view?usp=sharing",
       outsideLink: true,
     },
   ];
